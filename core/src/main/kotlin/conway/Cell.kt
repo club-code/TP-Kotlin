7b9de06e696ea4b574ee35d67f06f4cd5d189e56
khatharsis <hguelque@gmail.com>
2023-01-31 15:42:01 +0100

    Better Cell documentation.

--- core/src/main/kotlin/conway/Cell.kt
+++ core/src/main/kotlin/conway/Cell.kt
@@ -2,6 +2,9 @@ package conway
 
 import space.earlygrey.shapedrawer.ShapeDrawer
 
+/**
+ * Represents a Cell in the Universe.
+ */
 class Cell {
     fun draw(shapeDrawer: ShapeDrawer) {
         TODO("Draw the shape")
