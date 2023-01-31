98177446c90746ef15f9642e6ec92cef1ac18c64
khatharsis <hguelque@gmail.com>
2023-01-31 16:53:12 +0100

    Question 2

--- core/src/main/kotlin/conway/Universe.kt
+++ core/src/main/kotlin/conway/Universe.kt
@@ -6,11 +6,14 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer
  * A Conway universe. It's basically a grid, but we'd rather use a Set.
  */
 class Universe(val width: Int, val height: Int, val population: Int) {
+    var cellSet = List<Cell>(population) {
+        Cell((0 until width).random(), (0 until height).random())
+    }.toSet()
     /**
      * Draws all the cells contained in this universe, using the shapeDrawer.
      */
     fun draw(shapeRenderer: ShapeRenderer) {
-        TODO("Draw all the cells.")
+        cellSet.forEach { it.draw(shapeRenderer) }
     }
 
     /**
