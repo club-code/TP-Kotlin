b9551acc5083bf6712418c7417a3003962db9ab6
khatharsis <hguelque@gmail.com>
2023-02-02 16:47:05 +0100

    Modified.

--- src/main/kotlin/answer/Question6.kt
+++ src/main/kotlin/answer/Question6.kt
@@ -1,10 +1,9 @@
 package answer
 
-import monster.Size
 
 sealed class MonsterQ6 (
     val name: String,
-    val size: Size,
+    val size: SizeQ4,
     val location: String
 ): Comparable<MonsterQ6>  {
     override fun compareTo(other: MonsterQ6): Int =
